5766272f62afb26055fd38dd1457802d936edbf4
Neeraj Ahire <64035068+niirrr777@users.noreply.github.com>
2020-10-17 22:18:11 +0530

    Update reverse_number.c

--- reverse_number.c
+++ reverse_number.c
@@ -1,17 +1,10 @@
 /*.Write a program which accept number from user and display its digits in reverse
 order.
-Input :
-Output : 2395
-5
-9
-3
-2
-Input :
-Output : 1018
-8
-1
-0
-1*/
+Input :2395
+Output : 5932
+Input :1018
+Output : 8101
+*/
 
 
 #include<stdio.h>
